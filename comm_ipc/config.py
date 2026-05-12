52b5791d814d8561db1538b4075e9504f42c4717
shashstormer <shashanka5398@gmail.com>
2026-05-12 19:41:51 +0530

    bump v0.1.15 added better default path resolution for simpler background running and usage

--- comm_ipc/config.py
+++ comm_ipc/config.py
@@ -1,4 +1,20 @@
-SOCKET_PATH = "/tmp/comm_ipc.sock"
+import getpass
+import os
+
+
+def resolve_comm_ipc_path():
+    if env_path := os.getenv("COMM_IPC_SOCKET"):
+        return env_path
+
+    runtime_dir = os.getenv("XDG_RUNTIME_DIR")
+    if runtime_dir and os.path.isdir(runtime_dir):
+        return os.path.join(runtime_dir, "comm-ipc.default.sock")
+
+    user = getpass.getuser()
+    return os.path.join("/tmp", f"comm-ipc.{user}.sock")
+
+
+SOCKET_PATH = resolve_comm_ipc_path()
 DEFAULT_IDLE_TIMEOUT = 60.0
 DEFAULT_DATA_TIMEOUT = 60.0
 DEFAULT_HEARTBEAT_INTERVAL = 30.0
